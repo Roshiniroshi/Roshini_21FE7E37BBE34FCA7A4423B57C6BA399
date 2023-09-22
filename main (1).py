100f9ed0942e7abde3c7bf8ff9f6b1807de10147
Roshiniroshi <144543685+Roshiniroshi@users.noreply.github.com>
2023-09-22 20:21:24 +0530

    Add files via upload

--- main (1).py
+++ main (1).py
@@ -1,49 +1,27 @@
-'''Implement a class called Bank Account that represents a bank account.the class should have private
-Attributes for account number, account holder name, and account balance.Includes methods to
-deposit money, withdraw money, and display the account balance.Ensure that the account balance
-cannot be accessed directly from outside the class. Write a program to create an instance of the 
-BankAccount class and test the deposit and withdrawal functionality'''
+class Student:
+  
+  def __init__(self, name, roll_number, cgpa):
+    self.name = name
+    self.roll_number = roll_number
+    self.cgpa = cgpa
 
+def sort_students(student_list):
+  # Sort the list of students in descending order of CGPA
+  sorted_students = sorted(student_list, key=lambda student: student.cgpa, reverse=True)
 
-class BankAccount:
+ # Syntax - Lambda arg:exp
+  return sorted_students
 
-  def __init__(self, account_number, account_holder_name,initial_balance=0.0):
-    self.__account_number = account_number 
-    self.__account_holder_name = account_holder_name
-    self.__account_balance = initial_balance
+# Example usage:
+students = [
+  Student("Hari", "A123", 7.8),
+  Student("Srikanth", "A124", 8.2),
+  Student("Sowmiya", "A125", 9.1),
+  Student("Malar", "A126", 9.9),
+]
 
-  def deposit(self, amount) :
-    if amount > 0:
-      self.__account_balance += amount
-      #self.__account_balance = self.__account_balance+amount
-      print("Deposited ₹{}. New balance: ₹{}".format(amount, 
-                                    self.__account_balance))
-    else:
-      Print("Invalid deposit amount.")
+sorted_students = sort_students(students)
 
-  def withdraw(self, amount): 
-    if amount > 0 and amount <= self.__account_balance:
-      self.__account_balance -= amount
-      #self.__account_balance = self.__account_balance - amount
-      print("withdrew ₹{}. New balance: ₹{}".format(amount,
-                                    self.__account_balance))
-    else:
-      print("Invalid withdrawal amount or insufficient balance.")
-
-  def display_balance(self):
-    print("Account balance for {} (Account #{}): ₹{}".format(
-        self.__account_holder_name, self.__account_number,
-        self.__account_balance))
-
-
-# create an instance of the BankAccount class
-account = BankAccount(account_number="123456789",
-          account_holder_name="Hari Prabhu",
-            initial_balance=5000.0)
-
-# Test deposit and withdrawal functionality
-account.display_balance()
-account.deposit(500.0)
-account.withdraw(200.0)
-account.withdraw(20000.0)
-account.display_balance()
+# Print the sorted list of students
+for student in sorted_students:
+  print("Name: {}, Roll Number: {},  CGPA: {}".format(student.name, student.roll_number, student.cgpa))
--- main.py
+++ main.py
@@ -1,29 +1,11 @@
-''' Implement a class called Players that represents a cricket player.The Player class should have a
-method called play()which prints "The player is playing cricket.Drive two classes, Batsman and
-Bowler, from the Player class.Override the play()method in each derived class to print " The batsman
-is batting" and "The bowler is bowling", respectively.Write a program to create objects of both the
-Batsman and Bowler classes and call the play() method for each object.'''
+def linearSearchProduct(productList, targetProduct):
+  indices=[]
+  for index, product in enumerate(productList):
+    if product == targetProduct:
+      indices.append(index)
+  return indices
 
-
-# Define line base class Player 
-class Player:
-    def play(self):
-        print("The player is playing cricket.")
-
-# Define the derived class Batsman 
-class Batsman(Player):
-    def play(self): 
-        print("The batsman is batting.")
-
-# Define the derived class Bowler
-class Bowler(Player):
-      def play(self): 
-            print("The bowler is bowling.")
-
-# Create objects of Batsman and Bowler Classes 
-batsman = Batsman()
-bowler = Bowler()
-
-# Call the play() method for each object
-batsman.play()
-bowler.play()
+products = ["shoes","boat","loafer","shoes","sandal","shoes"]
+target = "shoes"
+result = linearSearchProduct(products, target)
+print(result)
